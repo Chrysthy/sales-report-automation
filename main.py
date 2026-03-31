03582615d7931a4826c802fb55b53e1836b0fd5b
Chrystine Martins <chrystine.martins@outlook.com>
2026-03-31 09:22:29 -0300

    feat: adiciona automação para acessar link específico e baixar o aqrquivo

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ pyautogui.press('enter')
 
 time.sleep(2)
 
+link = 'https://drive.google.com/drive/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga'
+
+pyautogui.write(link)
+pyautogui.press('enter')
+time.sleep(3)
+
+pyautogui.click(x=483, y=607, clicks=2)
+pyautogui.click(x=595, y=595)
+time.sleep(2)
+pyautogui.click(x=678, y=620)
+time.sleep(3)
